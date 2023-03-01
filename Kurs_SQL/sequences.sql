b10db3572ad7dda02bdbc5d866bed1656e754ff8
Mateusz Gula <mateuszgua@wp.eu>
2023-03-01 12:23:30 +0100

    Add excercises for add sequenses

--- Kurs_SQL/sequences.sql
+++ Kurs_SQL/sequences.sql
@@ -1,1 +1,25 @@
 create sequence test;
+
+select test.nextval
+from dual;
+
+select test.currval
+from dual;
+
+create sequence test2
+increment by 2
+maxvalue 10
+minvalue 5
+;
+
+create table testowa(a numeric, b varchar(10));
+
+create or replace trigger trigger_test
+before insert on testowa
+for each row
+declare 
+begin
+	:new.a := test.nextval;
+end;
+
+select * from testowa;
